c88a42e7db0e2f0dca9510b2d54045ec6d0b21ff
Prof Monaliza <83433815+profmonaliza@users.noreply.github.com>
2023-03-30 20:16:40 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,24 +55,3 @@ World.add(world,con);
   rectMode(CENTER);
   ellipseMode(RADIUS);
 }
-
-function draw() 
-{
-  background(51);
-  Engine.update(engine);
-  
-  
-
-  ellipse(ball.position.x,ball.position.y,20);
- // ground.show();
-  strokeWeight(2);
-  stroke(255);
-  line(con.pointA.x,con.pointA.y,ball.position.x,ball.position.y);
-  Engine.update(engine);
-}
-
-
-function vForce()
-{
-  Matter.Body.applyForce(ball,{x:0,y:0},{x:0.05,y:0});}
- 
